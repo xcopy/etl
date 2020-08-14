7f6d333c6286d1d981d18892cd9a4c1ba516ddb2
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-08-14 16:18:21 +0600

    Configuration via .env file

--- config/db.php
+++ config/db.php
@@ -1,10 +1,15 @@
 <?php
 
+$connection = getenv('DB_CONNECTION');
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$database = getenv('DB_DATABASE');
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => $connection.':host='.$host.';port='.$port.';dbname='.$database,
+    'username' => getenv('DB_USERNAME'),
+    'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
